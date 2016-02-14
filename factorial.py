5c83e3ff73f4203e30c58aeb63ab03c0b746ec02
zess1982 <zess1982@gmail.com>
2016-02-14 11:39:49 +0800

    edit factorial.py

--- factorial.py
+++ factorial.py
@@ -1,5 +1,11 @@
 def factorial(n):
-    if n==0:
+    if not isinstance(n, int):
+        print "Factorial is only defined for integers."
+        return none
+    elif n < 0:
+        print "Factorial is not defined for negative integers."
+        return none
+    elif n==0:
         return 1
     else:
         result = n * factorial(n-1)
